423b267366ba8cb213ee9238c97d66bad11352ae
Crispim <raissa.priscila#include iostream#include iostream#include iostream@gmail.com>
2021-07-08 19:23:27 -0300

    questoes de ordenacao

--- questao13.cpp
+++ questao13.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-int compare(const void *a, const void *b){
-    if(*(float*)a == *(float*)b){
-        return 0;
+int y[900];
+void compare(int x[900], int n){
+
+    int aux;
+
+    for(int j = 0; j<n; j++){
+        for(int i = 0; i<n; i++){
+            if(x[i]>x[j]){
+                aux = x[i];
+                x[i] = x[j];
+                x[j] = aux;
+            }
+        }
     }
-    else if(*(float*)a > *(float*)b){
-        return 1;
-    }else{
-        return -1;
+    for (int i = 0; i < n; i++)
+    {
+        y[i] = x[i];
     }
+    
 }
 int main(int argc, char const *argv[])
 {
-    int n;
+    int n, *x;
 
     printf("Didite quantos valores sao: ");
     scanf("%d", &n);
-
-    float *x;
     
-    x = (float*) malloc (n*sizeof(float));
+    x = (int*) malloc (n*sizeof(int));
     for(int i = 0; i<n; i++){
         printf("Digite os valores: ");
         cin >> x[i];
     }
-    qsort(x,n,sizeof(float), compare);
+    compare(x,n);
     for (int i = 0; i < n; i++)
     {
-        printf("%.2f ", x[i]);
+        printf("%d ", y[i]);
     }
     
     free(x);
